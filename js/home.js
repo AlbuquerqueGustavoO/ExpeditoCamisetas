d777a3b5dded55011087d5bf4c988753c9b06f7d
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-03-04 18:54:54 -0300

    Detalhes produto

--- js/home.js
+++ js/home.js
@@ -2,4 +2,12 @@ $(function() {
     $('#idNavbar').click(function(){
         $('.colapseGenerico').toggle();
     });
+
+    $(".zoom_05").elevateZoom({
+        zoomType				: "lens",
+        lensShape : "round",
+        lensSize    : 100
+      });
+      
+    $("#carouselExampleCaptions").carousel('pause');
 });
